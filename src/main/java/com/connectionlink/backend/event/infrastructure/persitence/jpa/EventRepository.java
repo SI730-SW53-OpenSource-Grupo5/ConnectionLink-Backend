f4379838b8bb6641e9fb96a10a6a285c7a39418a
Djalma Dioses <54141599+djalmadioses@users.noreply.github.com>
2024-05-30 23:08:16 -0500

    feat: add repository with methods

---
+++ src/main/java/com/connectionlink/backend/event/infrastructure/persitence/jpa/EventRepository.java
@@ -0,0 +1,17 @@
+package com.connectionlink.backend.event.infrastructure.persitence.jpa;
+
+import com.connectionlink.backend.event.domain.model.aggregates.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findAll();
+
+    List<Event> findByUsersId(Long userId);
+
+    List<Event> findByCategoryId(Long categoryId);
+    Optional<Event> findById(Long id);
+    List<Event> findByUsersUsername(String username);
+}
