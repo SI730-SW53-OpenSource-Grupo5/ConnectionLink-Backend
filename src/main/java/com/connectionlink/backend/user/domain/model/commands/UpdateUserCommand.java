f679d37a0072d6fbb70e67c02f19fa356229a883
Djalma Dioses <54141599+djalmadioses@users.noreply.github.com>
2024-05-30 22:59:14 -0500

    feat: add user entity and create and update command

---
+++ src/main/java/com/connectionlink/backend/user/domain/model/commands/UpdateUserCommand.java
@@ -0,0 +1,29 @@
+package com.connectionlink.backend.user.domain.model.commands;
+
+import java.util.Date;
+
+public record UpdateUserCommand(String fullName, String description, String profileImageUrl, String bannerImageUrl, Integer age, Date birthday, String cvUrl) {
+    public UpdateUserCommand {
+        if (fullName == null || fullName.isBlank()) {
+            throw new IllegalArgumentException("fullName cannot be null or empty");
+        }
+        if (description == null) {
+            throw new IllegalArgumentException("description cannot be null");
+        }
+        if (profileImageUrl == null ) {
+            throw new IllegalArgumentException("profileImageUrl cannot be null");
+        }
+        if (bannerImageUrl == null) {
+            throw new IllegalArgumentException("bannerImageUrl cannot be null");
+        }
+        if (age == null || age <= 0) {
+            throw new IllegalArgumentException("age must be a positive integer");
+        }
+        if (birthday == null) {
+            throw new IllegalArgumentException("birthday cannot be null");
+        }
+        if (cvUrl == null ) {
+            throw new IllegalArgumentException("cvUrl cannot be null");
+        }
+    }
+}
